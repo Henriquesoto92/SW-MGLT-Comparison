edd0eedd27f15c8496464685c83b9519dafd8794
henriquesoto92 <henriquesoto92@gmail.com>
2023-07-08 20:05:59 -0300

    feat: final project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Comparator from "@/components/Comparator";
 
 export default function Home() {
   return (
-    <main className="flex gap-4 min-h-screen flex-col items-center p-20 mt-10 text-gray-400">
+    <main className="flex gap-4 min-h-screen flex-col items-center p-20 sm:pt-[120px] pt-[200px] text-gray-400">
       <h1>
         How many stops would it take if a Star Wars spaceship needs to travel
         the distance below the input below
