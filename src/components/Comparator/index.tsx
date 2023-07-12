1a79d28184c72f9a039faa9728af2251a45b6abf
henriquesoto92 <henriquesoto92@gmail.com>
2023-07-11 21:20:13 -0300

    feat: add loading videos in fetch

--- src/components/Comparator/index.tsx
+++ src/components/Comparator/index.tsx
@@ -8,18 +8,20 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { use, useState } from "react";
+import { useState } from "react";
 import InputDistance from "./InputDistance";
 import { useStarships } from "@/services/hooks/useStarships";
 import { formatMGLT } from "@/services/utils/FormatMGLT";
 
 export default function Comparator() {
-  const { data: starshipData, isFetched } = useStarships();
+  const { data: starshipData, isFetched, isError } = useStarships();
   const [distance, setDistance] = useState<number | null>(10000);
+
+  console.log(isError);
 
   return (
     <div className="flex flex-col gap-5">
-      {isFetched ? (
+      {isFetched && !isError ? (
         <>
           <InputDistance setDistance={setDistance} distance={distance} />
           <div className="rounded-md border">
@@ -47,12 +49,42 @@ export default function Comparator() {
           </div>
         </>
       ) : (
-        <Loading />
+        <Loading isError={isError} />
       )}
     </div>
   );
 }
 
-function Loading() {
-  return <h2>preparing hyperdrive...</h2>;
+type propsLoading = {
+  isError: boolean;
+};
+
+function Loading({ isError }: propsLoading) {
+  return (
+    <h2>
+      {isError ? (
+        <div className="flex items-center flex-col gap-4">
+          sorry, we miscalculated...
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/4F4qzPbcFiA?controls=0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
+      ) : (
+        <div className="flex items-center flex-col gap-4">
+          preparing hyperdrive...
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/6ga4IICXyCE?controls=0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
+      )}
+    </h2>
+  );
 }
